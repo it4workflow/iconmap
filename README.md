b87d4c0fa509195d321dd039d721a70558c2bd20
unknown <hartmannh@W1OG334570.haba.int>
2014-10-27 11:33:33 +0100

    Removed all atm specific codings and replaced with more generic iconmap codings

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# atm-map
+# iconmap
 
-check out the <a href="http://www.marcusbleil.de/m/">demo</a>
+inspired by https://github.com/momolinus/atm-map
+
+removed all atm specific coding to make a blueprint for other use cases
 
 icons used by this project:
 <a href="http://mapicons.nicolasmollet.com/">Nicolas Mollet</a> <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY SA 3.0</a>
@@ -11,14 +13,3 @@ permalink: https://github.com/shramov/leaflet-plugins
 
 Photon search plugin: https://github.com/komoot/leaflet.photon
 
-## Version 1.1
-
-published 01.09.2014
-
--some enhancement
-
-## Version 1.0
-
-published 27.07.2014
-
--fixed error with "way-nodes" has individual tags
---
+++ webcontent/iconmaputils.js
@@ -0,0 +1,81 @@
+/**
+ * @author Marcus
+ */
+var UTILS = {};
+
+(function() {
+
+	var buildIcon = function(iconPic) {
+		return L.icon({
+			iconUrl : iconPic,
+			iconSize : [ 32, 37 ],
+			iconAnchor : [ 15, 37 ],
+			popupAnchor : [ -3, -37 ],
+			labelAnchor : [ 10, -18 ],
+		});
+	};
+
+	UTILS.latLongToString = function(bounds) {
+
+		/* The coordinate order is (lower lat, lower lon, upper lat, upper lon) */
+		var result;
+		var lowerLat, lowerLng, upperLat, upperLng;
+
+		lowerLat = bounds.getSouthWest().lat;
+		lowerLng = bounds.getSouthWest().lng;
+		upperLat = bounds.getNorthEast().lat;
+		upperLng = bounds.getNorthEast().lng;
+
+		result = lowerLat + "," + lowerLng + "," + upperLat + "," + upperLng;
+
+		return result;
+	};
+
+    UTILS.createNameFromeTags = function(node) {
+		if (node.tags.ref) {
+			return 'ref: ' + node.tags.ref;
+		} else if (node.tags.name) {
+			return 'name: ' + node.tags.name;
+		} else {
+			return "?";
+		}
+    };
+
+	UTILS.addLegendTo = function(map) {
+
+		var legend = L.control({
+			position : 'topright',
+			collapsed : true
+		});
+
+		legend.onAdd = function(map) {
+
+		    var div = L.DomUtil.create('div', 'legend');
+		    var table = '';
+
+		    table += '<p id="legend_but" style="cursor: pointer">Legende (+/-):</p>';
+		    table += '<div id="legend_table">'
+		    table += '<table>';
+		    table += '<tr>';
+		    table += '<td>';
+		    table += 'legend';
+		    table += '</td>';
+		    table += '</tr>';
+
+		    table += '</table>';
+		    table += '</div>';
+		    
+		    div.innerHTML += table;
+		    
+		    return div;
+		};
+
+		legend.addTo(map);
+
+		$('#legend_but').on('click', function() {
+			$('#legend_table').toggle();
+		});
+
+		$('#legend_table').toggle();
+	};
+})();
